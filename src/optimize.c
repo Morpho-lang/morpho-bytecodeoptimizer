b4e58a84ee8209bc63d453bdfc45b0c43d897b47
softmattertheory <timothy.atherton@tufts.edu>
2024-11-29 16:17:09 -0500

    Add in old optimizer code from 0.5.7

--- src/optimize.c
+++ src/optimize.c
@@ -1,7 +1,12 @@
+/** @file optimize.c
+ *  @author T J Atherton
+ *
+ *  @brief Optimizer for compiled morpho bytecode
+*/
 
 #include <morpho/morpho.h>
 #include "optimize.h"
 
 void bytecodeoptimizer_initialize(void) {
-    
+    printf("Optimizer loaded\n");
 }
--- src/optimize.h
+++ src/optimize.h
@@ -1,5 +1,8 @@
-
-
+/** @file optimize.h
+ *  @author T J Atherton
+ *
+ *  @brief Optimizer for compiled morpho bytecode
+*/
 
 void bytecodeoptimizer_initialize(void);
 
