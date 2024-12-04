ce126b8934a8f8d5ae2f6fc25814ae8872592f04
softmattertheory <timothy.atherton@tufts.edu>
2024-12-04 09:30:14 -0500

    layout skeleton

---
+++ src/layout.c
@@ -0,0 +1,8 @@
+/** @file layout.c
+ *  @author T J Atherton
+ *
+ *  @brief Layout final program from control flow graph
+*/
+
+#include "morphocore.h"
+#include "layout.h"
---
+++ src/layout.h
@@ -0,0 +1,10 @@
+/** @file layout.h
+ *  @author T J Atherton
+ *
+ *  @brief Layout final program from control flow graph
+*/
+
+#ifndef layout_h
+#define layout_h
+
+#endif
